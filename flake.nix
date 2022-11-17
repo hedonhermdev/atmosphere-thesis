1a0193fa3de553e99f8193a69e857b27dbaa5a45
Tirth Jain <jaintirth24@gmail.com>
2022-11-17 21:47:12 +0000

    refactor + initial writing

---
+++ flake.nix
@@ -0,0 +1,25 @@
+{
+  description = "A simple project";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils, ... }: let
+    # System types to support.
+    supportedSystems = [ "x86_64-linux" "aarch64-linux" ];
+
+    # Rust nightly version.
+    nightlyVersion = "2022-11-06";
+  in flake-utils.lib.eachSystem supportedSystems (system: let
+    pkgs = nixpkgs.legacyPackages.${system};
+  in {
+    devShell = pkgs.mkShell {
+      nativeBuildInputs = with pkgs; [
+        texlive.combined.scheme-full
+        texlab
+      ];
+    };
+  });
+}
